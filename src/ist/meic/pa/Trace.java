84c1b492e6e79a7be2d6d193260f2a6fcee1eb14
Samuel Coelho <samuelfcmc@gmail.com>
2014-04-23 10:11:13 +0100

    Initialized project structure. Just a small step for the programmer but a giant one for the humanity

---
+++ src/ist/meic/pa/Trace.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ist.meic.pa;
+
+/**
+ * 
+ *
+ */
+public class Trace {
+
+	public static void print(Object object) {
+		/*
+		 * TODO: Implement this thing...
+		 */
+	}
+}
---
+++ src/ist/meic/pa/TraceVM.java
@@ -0,0 +1,13 @@
+package ist.meic.pa;
+
+public class TraceVM {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
