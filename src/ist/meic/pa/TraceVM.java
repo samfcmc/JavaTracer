db1c2eac7e767dded8852e969fbdbc604673dddb
firedeathz <mikazzz_pro@hotmail.com>
2014-04-30 11:57:02 +0100

    Implemented TraceVM to run other main methods

--- src/ist/meic/pa/TraceVM.java
+++ src/ist/meic/pa/TraceVM.java
@@ -1,14 +1,29 @@
 package ist.meic.pa;
 
+import java.io.PrintStream;
+import java.lang.reflect.*;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.NotFoundException;
 
 public class TraceVM {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static PrintStream printStream = System.err;
+
+	public static void main(String[] args) throws NotFoundException,
+			CannotCompileException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		if (args.length < 1) {
+			printStream.println("Invalid number of arguments.");
+			printStream.println("Usage: TraceVM <filename>");
+		} else {
+			ClassPool pool = ClassPool.getDefault();
+			CtClass ctClass = pool.get(args[0]);
+			Class<?> rtClass = ctClass.toClass();
+			Method main = rtClass.getMethod("main", args.getClass());
+			main.invoke(null, new Object[] {});
+		}
 	}
 
 }
