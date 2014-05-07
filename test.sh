e8d3a8a2f7c614bd2d6cc90f89ca19f3925e0a49
Samuel Coelho <samuelfcmc@gmail.com>
2014-05-07 10:09:50 +0100

    Fixed tests script

--- test.sh
+++ test.sh
@@ -7,19 +7,24 @@ TESTS_DIR="tests"
 TEST_CLASSES_DIR="$TESTS_DIR/classes"
 EXPECTED_OUTPUT_DIR="$TESTS_DIR/expected"
 OUTPUT_DIR="$TESTS_DIR/out"
-CP="libs/javassist.jar;tracer.jar"
+CP=".:libs/javassist.jar:tracer.jar"
 
 TEST_CLASS_1="Test2"
 TEST_CLASS_1_EXPECTED="test2.expected"
+
+echo "Creating directory for output"
+mkdir $OUTPUT_DIR
+
 # Compile and generate tracer.jar
 echo "[INFO] Compiling and generating the jar"
 ant
 
 echo "[INFO] Compile test class $TEST_CLASS_1"
-javac -cp $CP $TEST_CLASSES_DIR/$TEST_CLASS_1.java
+cp $TEST_CLASSES_DIR/$TEST_CLASS_1.java .
+javac -cp $CP $TEST_CLASS_1.java
 
 echo "[INFO] Run tracer against test class $TEST_CLASS_1"
-java -jar $JAR_NAME $TEST_CLASSES_DIR/$TEST_CLASS_1 2> $OUTPUT_DIR/test2.out
+java -cp $CP ist.meic.pa.TraceVM $TEST_CLASS_1 >& $OUTPUT_DIR/test2.out
 
 echo "[INFO] Comparing results with expected output"
 diff $EXPECTED_OUTPUT_DIR/$TEST_CLASS_1_EXPECTED $OUTPUT_DIR/test2.out
