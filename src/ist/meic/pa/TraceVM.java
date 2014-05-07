fe227f8c77a63018f061bbb837f7fdb77a79012b
Samuel Coelho <samuelfcmc@gmail.com>
2014-05-07 22:48:52 +0100

    Refactored the code to avoid duplicated code in TraceVM and TraceVMExtended

--- src/ist/meic/pa/TraceVM.java
+++ src/ist/meic/pa/TraceVM.java
@@ -1,25 +1,9 @@
 package ist.meic.pa;
 
-import java.io.PrintStream;
-import javassist.*;
 
 public class TraceVM {
 
-	private static PrintStream printStream = System.err;
-
-	public static void main(String[] args) throws Throwable {
-		if (args.length < 1) {
-			printStream.println("Invalid number of arguments.");
-			printStream.println("Usage: TraceVM <filename>");
-		} else {
-			Translator translator = new TraceTranslator();
-			ClassPool pool = ClassPool.getDefault();
-			Loader classLoader = new Loader();
-			classLoader.delegateLoadingOf("ist.meic.pa.Trace");
-			classLoader.addTranslator(pool, translator);
-			String[] restArgs = new String[args.length - 1];
-			System.arraycopy(args, 1, restArgs, 0, restArgs.length);
-			classLoader.run(args[0], restArgs); //throws throwable...
-		}
+	public static void main(String[] args) {
+		Tracer.run(args, new TraceTranslator());
 	}
 }
---
+++ src/ist/meic/pa/Tracer.java
@@ -0,0 +1,44 @@
+package ist.meic.pa;
+
+import java.io.PrintStream;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.Loader;
+import javassist.NotFoundException;
+import javassist.Translator;
+
+public class Tracer {
+
+	private static PrintStream printStream = System.err;
+
+	public static void run(String mainArgs[], Translator translato) {
+		if (mainArgs.length < 1) {
+			printStream.println("Invalid number of arguments.");
+			printStream.println("Usage: TraceVM <filename>");
+		} else {
+			Translator translator = new TraceTranslator();
+			ClassPool pool = ClassPool.getDefault();
+			Loader classLoader = new Loader();
+			classLoader.delegateLoadingOf("ist.meic.pa.Trace");
+			try {
+				classLoader.addTranslator(pool, translator);
+			} catch (NotFoundException e) {
+				printStream.println("ERROR: Class " + e.getClass().getName()
+						+ " not found");
+			} catch (CannotCompileException e) {
+				printStream.println("ERROR: Cannot compile injected code");
+			}
+			String[] restArgs = new String[mainArgs.length - 1];
+			System.arraycopy(mainArgs, 1, restArgs, 0, restArgs.length);
+			try {
+				classLoader.run(mainArgs[0], restArgs);
+			} catch (Throwable e) {
+				// I know that this error message says nothing but we are
+				// catching a throwable
+				printStream
+						.println("ERROR: An error ocurred while running the class loader");
+			}
+		}
+	}
+}
