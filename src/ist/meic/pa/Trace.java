aa48b1546cd54d8e56b02b9176b7d18f3cbec77d
Samuel Coelho <samuelfcmc@gmail.com>
2014-05-07 23:33:21 +0100

    Added doc target to generate javadoc of Trace class

--- src/ist/meic/pa/Trace.java
+++ src/ist/meic/pa/Trace.java
@@ -2,13 +2,42 @@ package ist.meic.pa;
 
 import java.io.PrintStream;
 
+/**
+ * The Class Trace. This class provides an interface to the programmer to trace
+ * whatever objects in some different ways
+ */
 public class Trace {
 
-	private static PrintStream printStream = System.err;
+	/** The default print stream. */
+	private static PrintStream defaultPrintStream = System.err;
+
+	/** The Constant NO_HISTORY. */
 	private static final String NO_HISTORY = "Tracing for %s is nonexistent!";
+
+	/** The Constant OBJECT_HISTORY. */
 	private static final String OBJECT_HISTORY = "Tracing for %s";
 
+	/**
+	 * Prints the tracing for Object <code>object</code> in the default print
+	 * stream which is the <code>System.err</code>.
+	 * 
+	 * @param object
+	 *            the object
+	 */
 	public static void print(Object object) {
+		print(object, defaultPrintStream);
+	}
+
+	/**
+	 * Prints the tracing for Object <code>object</code> in the given print
+	 * stream <code>printStream</code>.
+	 * 
+	 * @param object
+	 *            the object
+	 * @param printStream
+	 *            the print stream
+	 */
+	public static void print(Object object, PrintStream printStream) {
 		History objectHistory = Tracer.getHistory(object);
 
 		if (objectHistory == null) {
@@ -18,6 +47,21 @@ public class Trace {
 			String message = String.format(OBJECT_HISTORY, object.toString());
 			printStream.println(message);
 			objectHistory.print(printStream);
+		}
+	}
+
+	/**
+	 * Prints the tracing for Object <code>object</code> in all print streams in
+	 * <code>printStreams</code>.
+	 * 
+	 * @param object
+	 *            the object
+	 * @param printStreams
+	 *            the print streams
+	 */
+	public static void print(Object object, Iterable<PrintStream> printStreams) {
+		for (PrintStream printStream : printStreams) {
+			print(object, printStream);
 		}
 	}
 
