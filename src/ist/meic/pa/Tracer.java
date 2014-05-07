30dcad33616b0dec1f0b3ad12ba2f85c1e7493b3
Samuel Coelho <samuelfcmc@gmail.com>
2014-05-07 23:09:32 +0100

    Moved the responsability to manage tracing information to Tracer class

--- src/ist/meic/pa/Tracer.java
+++ src/ist/meic/pa/Tracer.java
@@ -1,6 +1,15 @@
 package ist.meic.pa;
 
+import ist.meic.pa.historyelements.CastHistoryElement;
+import ist.meic.pa.historyelements.Direction;
+import ist.meic.pa.historyelements.FieldAccessHistoryElement;
+import ist.meic.pa.historyelements.HistoryElement;
+import ist.meic.pa.historyelements.ReturnHistoryElement;
+import ist.meic.pa.historyelements.UsedAsArgumentHistoryElement;
+
 import java.io.PrintStream;
+import java.util.IdentityHashMap;
+import java.util.Map;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -9,6 +18,8 @@ import javassist.NotFoundException;
 import javassist.Translator;
 
 public class Tracer {
+
+	private static Map<Object, History> history = new IdentityHashMap<Object, History>();
 
 	private static PrintStream printStream = System.err;
 
@@ -41,5 +52,62 @@ public class Tracer {
 						.println("ERROR: An error ocurred while running the class loader");
 			}
 		}
+	}
+
+	public static History getHistory(Object object) {
+		return history.get(object);
+	}
+
+	public static void createHistory(Object object) {
+		if (!history.containsKey(object)) {
+			history.put(object, new History());
+		}
+	}
+
+	public static void addReturnElementToHistory(Object object,
+			String methodName, String fileName, int lineNumber) {
+		createHistory(object);
+		HistoryElement element = new ReturnHistoryElement(methodName, fileName,
+				lineNumber);
+		getHistory(object).addHistoryElement(element);
+	}
+
+	public static void addUsedAsArgumentElementToHistory(Object[] objects,
+			String methodName, String fileName, int lineNumber) {
+		HistoryElement historyElement = new UsedAsArgumentHistoryElement(
+				methodName, fileName, lineNumber);
+		for (Object object : objects) {
+			createHistory(object);
+			getHistory(object).addHistoryElement(historyElement);
+		}
+	}
+
+	public static void addCastElementToHistory(Object object,
+			Class<?> castType, String fileName, int lineNumber) {
+		HistoryElement element = new CastHistoryElement(castType.getName(),
+				fileName, lineNumber);
+		createHistory(object);
+		getHistory(object).addHistoryElement(element);
+	}
+
+	public static void addFieldAccessElementToHistory(Object object,
+			String fieldName, String fileName, int lineNumber,
+			Direction direction) {
+		HistoryElement element = new FieldAccessHistoryElement(fieldName,
+				fileName, lineNumber, direction);
+		createHistory(object);
+		getHistory(object).addHistoryElement(element);
+	}
+
+	public static void addFieldSetElementToHistory(Object object,
+			String fieldName, String fileName, int lineNumber) {
+		addFieldAccessElementToHistory(object, fieldName, fileName, lineNumber,
+				Direction.IN);
+	}
+
+	public static void addFieldGetElementToHistory(Object object,
+			String fieldName, String fileName, int lineNumber) {
+		addFieldAccessElementToHistory(object, fieldName, fileName, lineNumber,
+				Direction.OUT);
 	}
 }
